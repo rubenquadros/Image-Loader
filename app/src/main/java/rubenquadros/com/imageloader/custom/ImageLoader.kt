76c305fbc1e3edabdfc05718137b611934abeb0e
Ruben Quadros <rquadros95@gmail.com>
2019-07-28 10:19:34 +0000

    crash handling

--- app/src/main/java/rubenquadros/com/imageloader/custom/ImageLoader.kt
+++ app/src/main/java/rubenquadros/com/imageloader/custom/ImageLoader.kt
@@ -2,7 +2,9 @@ package rubenquadros.com.imageloader.custom
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.widget.ImageView
 import rubenquadros.com.imageloader.callbacks.Callback
@@ -12,9 +14,22 @@ import rubenquadros.com.imageloader.services.BackgroundTask
 class ImageLoader(context: Context?) : ImageView(context), Callback {
 
     private var mImageURL: String? = null
-    private var mPlaceholder: String? = null
+    private var mPlaceholder: String? = "applogo"
     private lateinit var mDrawable: Drawable
     private var mBitmap: Bitmap? = null
+    private val bitmapResource: Bitmap?
+        get() {
+            if (mBitmap == null) {
+                val drawable = drawable ?: return null
+
+                return if (width == 0 || height == 0) {
+                    null
+                } else (drawable as BitmapDrawable).bitmap
+
+            } else {
+                return mBitmap
+            }
+        }
     private lateinit var backgroundTask: BackgroundTask
 
     constructor(context: Context?, attrs: AttributeSet?): this(context) {
@@ -29,19 +44,25 @@ class ImageLoader(context: Context?) : ImageView(context), Callback {
         mPlaceholder = typedArray.getString(R.styleable.ImageLoader_placeholder)
         mImageURL = typedArray.getString(R.styleable.ImageLoader_imageURL)
         typedArray.recycle()
-        if(mPlaceholder != null) {
-            try {
-                mDrawable = context.resources.getDrawable(
-                    resources.getIdentifier(
-                        this.mPlaceholder,
-                        "drawable",
-                        context.packageName
-                    )
-                )
-                setImageDrawable(mDrawable)
-            }catch (e: Exception) {
-
-            }
+//        if(mPlaceholder != null) {
+//            try {
+//                mDrawable = context.resources.getDrawable(
+//                    resources.getIdentifier(
+//                        this.mPlaceholder,
+//                        "drawable",
+//                        context.packageName
+//                    )
+//                )
+//                setImageDrawable(mDrawable)
+//            }catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//        }
+        try {
+            mDrawable = ContextCompat.getDrawable(context, android.R.drawable.progress_indeterminate_horizontal)!!
+            setImageDrawable(mDrawable)
+        }catch (e: Exception) {
+            e.printStackTrace()
         }
         if(mImageURL != null) {
             getBitmap(mImageURL!!)
